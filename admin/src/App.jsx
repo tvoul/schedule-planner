97f0cdc37253fc9b682dd6e71def291109c85a45
r_clementz <rie.h620@gmail.com>
2022-08-30 15:53:17 +0200

    import create/edit school added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,13 +2,15 @@
 import * as React from "react";
 import { Admin, Resource, EditGuesser } from 'react-admin';
 import { PostList } from './posts';
-import { PostEdit } from "./post-edit";
-import { PostCreate} from "./post-create";
+import { PostEdit, SchoolEdit } from "./post-edit";
+import { PostCreate, SchoolCreate} from "./post-create";
 import { UserList } from './users';
 import { TeacherList } from "./teachers";
 import { TeacherEdit } from "./post-edit";
 import { TeacherCreate } from "./post-create";
 import { SchoolList } from "./schools";
+import { SchoolEdit } from "./post-edit";
+import { SchoolCreate } from "./post-create";
 import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
@@ -24,8 +26,7 @@ const App = () => (
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
     <Resource name="teachers" list={TeacherList} icon={TeacherIcon} edit={TeacherEdit} create={TeacherCreate} />
-    <Resource name="teachers" list={TeacherList} icon={TeacherIcon} />
-    <Resource name="schools" list={SchoolList} icon={SchoolIcon} />
+    <Resource name="schools" list={SchoolList} icon={SchoolIcon} edit={SchoolEdit} create={SchoolCreate} />
     
   </Admin>  
 );
