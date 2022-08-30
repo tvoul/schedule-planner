12a2c2b01140f217818ba19eca76cbe9ac7bf473
r_clementz <rie.h620@gmail.com>
2022-08-30 13:48:15 +0200

    created school.jsx

---
+++ admin/src/schools.jsx
@@ -0,0 +1,14 @@
+import { Datagrid, EmailField, List, TextField} from 'react-admin';
+
+//Teachers: id, firstname.lastname, initials, phone,email,color,hide,password,roles
+// ?? Need filter for List to be able to serach teachers later on ?? (Different feature)
+export const SchoolList = () => (
+    <List>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="shortname" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
