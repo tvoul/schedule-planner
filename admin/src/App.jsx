61c4f194a9fad767ae27eeae46400525db1ee101
Jsvensson12 <Jespersvensson1@live.com>
2022-09-01 14:19:33 +0200

    Fixed post/create

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,9 +4,10 @@ import { Admin, Resource, EditGuesser } from 'react-admin';
 import { PostList } from './posts';
 import { TeacherList } from "./teachers";
 import { SchoolList } from "./schools";
-import { PostEdit, TeacherEdit, SchoolEdit } from "./post-edit";
-import { PostCreate, TeacherCreate, SchoolCreate} from "./post-create";
+import { PostEdit, TeacherEdit, SchoolEdit, ClassEdit } from "./post-edit";
+import { PostCreate, TeacherCreate, SchoolCreate, ClassCreate} from "./post-create";
 import { UserList } from './users';
+import { ClassList } from './classes';
 import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
@@ -21,7 +22,7 @@ const App = () => (
     <Resource name="users" list={UserList} icon={UserIcon} />
     <Resource name="teachers" list={TeacherList} icon={TeacherIcon} edit={TeacherEdit} create={TeacherCreate} />
     <Resource name="schools" list={SchoolList} icon={SchoolIcon} edit={SchoolEdit} create={SchoolCreate} />
-    
+    <Resource name="classes" list={ClassList} edit={ClassEdit} create={ClassCreate} />
   </Admin>  
 );
 // edit={PostTeachers} create={TeacherCreate} need to be added in follwoing sprint 
